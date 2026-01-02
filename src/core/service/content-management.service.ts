7081542f030d2e322babcc3b3aeca069580aa223
Joao Pedro <jjoao.monteiro15@gmail.com>
2026-01-02 10:07:29 -0300

    aplicando as entidades de dominio no service de criacao de conteudo

--- src/core/service/content-management.service.ts
+++ src/core/service/content-management.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from "@nestjs/common";
 import { VideoDAO } from "@src/persistence/dao/video.dao";
+import { ContentEntity, ContentType } from "../entity/content.entity";
+import { MovieEntity } from "../entity/movie.entity";
+import { VideoEntity } from "../entity/video.entity";
+import { ThumbnailEntity } from "../entity/thumbnail.entity";
 
 export interface CreateContentData {
   title: string
@@ -14,6 +18,21 @@ export class ContentManagementService {
   constructor(private readonly videoDAO: VideoDAO) { }
 
   async createContent(createContentData: CreateContentData) {
+    const content = ContentEntity.createNew({
+      title: createContentData.title,
+      description: createContentData.description,
+      type: ContentType.MOVIE,
+      media: MovieEntity.createNew({
+        video: VideoEntity.createNew({
+          url: createContentData.url,
+          sizeInKb: createContentData.sizeInKb,
+          duration: 100
+        }),
+        thumbnail: ThumbnailEntity.createNew({
+          url: createContentData.thumbnailUrl
+        })
+      })
+    })
     const createdVideo = await this.videoDAO.create(createContentData)
     return createdVideo
   }
