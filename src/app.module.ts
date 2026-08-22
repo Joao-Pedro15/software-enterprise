325dc46531abb9d48aeaa24bf7e7550b5db10d36
Joao Pedro <jjoao.monteiro15@gmail.com>
2026-08-22 16:32:20 -0300

    criando entidades de banco de dados e repositorios de persistencia

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from '@src/persistence/prisma/prisma.service';
 import { ContentController } from './http/rest/controller/content.controller';
 import { ContentManagementService } from './core/service/content-management.service';
 import { ContentRepository } from './persistence/repository/content.repository';
@@ -11,6 +10,6 @@ import { ConfigModule } from './infra/module/config/config.module';
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [ContentController, MediaPlayerController],
-  providers: [PrismaService, ContentManagementService, MediaPlayerService, ContentRepository, VideoRepository],
+  providers: [ContentManagementService, MediaPlayerService, ContentRepository, VideoRepository],
 })
 export class AppModule { }
